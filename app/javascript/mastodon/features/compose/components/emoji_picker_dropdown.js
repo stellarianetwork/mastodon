dc0ec7a95a71e00bd7775d4560924140bf14da3f
eai04191 <eai@mizle.net>
2019-04-24 11:52:18 +0900

    ebinize

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
@@ -379,7 +379,7 @@ class EmojiPickerDropdown extends React.PureComponent {
           {button || <img
             className={classNames('emojione', { 'pulse-loading': active && loading })}
             alt='🙂'
-            src={`${assetHost}/emoji/1f602.svg`}
+            src={`${assetHost}/emoji/1f990.svg`}
           />}
         </div>
 
