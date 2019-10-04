a93c80873948e72a9c07e2c82db01388299c6b4f
eai04191 <eai@mizle.net>
2019-10-04 15:40:15 +0900

    remove number formatting

--- app/javascript/mastodon/utils/numbers.js
+++ app/javascript/mastodon/utils/numbers.js
@@ -2,11 +2,5 @@ import React, { Fragment } from 'react';
 import { FormattedNumber } from 'react-intl';
 
 export const shortNumberFormat = number => {
-  if (number < 1000) {
-    return <FormattedNumber value={number} />;
-  } else if (number < 1000000) {
-    return <Fragment><FormattedNumber value={number / 1000} maximumFractionDigits={1} />K</Fragment>;
-  } else {
-    return <Fragment><FormattedNumber value={number / 1000000} maximumFractionDigits={1} />M</Fragment>;
-  }
+  return <FormattedNumber value={number} />;
 };
